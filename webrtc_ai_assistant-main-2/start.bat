af805dc549e71e8620c3c8dc5157239226420f26
brOOkzyz <971216172@qq.com>
2025-08-21 15:28:29 +0800

    Delete webrtc_ai_assistant-main-2 directory

--- webrtc_ai_assistant-main-2/start.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo 启动WebRTC语音助手...
-cd webrtc_voice_assistant
-python start.py
-pause
--- webrtc_ai_assistant-main-2/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "启动WebRTC语音助手..."
-cd webrtc_voice_assistant
-python3 start.py
